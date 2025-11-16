80d249df2808ae43799d0ee2312951baa6455eec
kelly <kiroegbu@gmail.com>
2025-11-16 21:14:53 +0100

    Update project files

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,21 +1,21 @@
 
-
-
-interface ButtonProps {
+export interface ButtonProps {
   title: string;
-  styles?: string;
+  styles?: string; // <-- Add this line
 }
 
 
 
-//import { ButtonProps } from "@/interfaces";
 
-//const Button: React.FC<ButtonProps> = ({ title, styles }) => {
-  //return (
-    //<button className={`px-4 py-2 bg-blue-600 text-white ${styles}`}>
+
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
+return (
+  <button className={`px-4 py-2 bg-blue-600 text-white ${styles}`}>
       {title}
-    //</button>
-  //);
-//};
+  </button>
+  );
+};
 
-//export default Button;
+export default Button;
